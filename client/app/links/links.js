2e1b2fc33344d795fdab710941dd157a9c8d53ee
Victor Mu <vic.mgs@gmail.com>
2016-10-15 10:26:38 -0700

    Got signout

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,8 +1,12 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
+.controller('LinksController', function ($scope, Links, Auth) {
   $scope.data = {};
   Links.getAll().then(function(data) {
     $scope.data.links = data;
   });
+
+  $scope.signout = function () {
+    Auth.signout();
+  };
 });
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,8 +1,12 @@
 angular.module('shortly.shorten', [])
 
-.controller('ShortenController', function ($scope, $location, Links) {
+.controller('ShortenController', function ($scope, $location, Links, Auth) {
   $scope.addLink = function (link) {
     Links.addOne(link);
     $scope.link = {url: ''};
+  };
+
+  $scope.signout = function () {
+    Auth.signout();
   };
 });
